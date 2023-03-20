8f816f729b63a4149dd2a670090ee2e15ab96596
madhushrivani <madhushrivani@gmail.com>
2023-03-20 18:09:11 +0530

    array1

---
+++ array.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main()
+{
+    int marks[4];
+
+    for (int i = 0; i < 4; i++)
+    {
+        printf("Enter the value of %d element of array\n", i);
+        scanf("%d", &marks[i]);
+    }
+    for (int i = 0; i < 4; i++)
+    {
+        printf("the value of %d element of array is %d\n", i, marks[i]);
+    }
+    
+    //marks[0]=89;
+    //printf("Marks of 1 studet is %d\n", marks[0]);
+    //marks[0]=838;
+    //marks[1]=544;
+    //marks[2]=356;
+    //marks[3]=646;
+    //printf("Marks of 1 student is %d\n", marks[0]);
+    return 0;
+}
