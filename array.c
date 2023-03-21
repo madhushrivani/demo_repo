ea1cdfd781923cac15fcd10fb080fc40ec0bf001
madhushrivani <madhushrivani@gmail.com>
2023-03-21 23:30:55 +0530

    helli

--- array.c
+++ array.c
@@ -2,16 +2,16 @@
 
 int main()
 {
-    int marks[4];
+    int marks[4]={23,55,67,45};
 
+   // for (int i = 0; i < 4; i++)
+    //{//
+      //  printf("Enter the value of %d element of array\n", i);
+      //  scanf("%d", &marks[i]);
+    //}
     for (int i = 0; i < 4; i++)
     {
-        printf("Enter the value of %d element of array\n", i);
-        scanf("%d", &marks[i]);
-    }
-    for (int i = 0; i < 4; i++)
-    {
-        printf("the value of %d element of array is %d\n", i, marks[i]);
+       printf("the value of %d element of array is %d\n", i, marks[i]);
     }
     
     //marks[0]=89;
