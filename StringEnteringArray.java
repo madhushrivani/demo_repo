a9f094cba73e6aac52477ccd1e70800e4457f0b6
madhushrivani <madhushrivani@gmail.com>
2023-04-10 11:07:37 +0530

    String enetring in array

---
+++ StringEnteringArray.java
@@ -0,0 +1,16 @@
+public class StringEnteringArray {
+    //public class stringenteringArray {
+
+        public static void main(String[] args) {
+            // TODO Auto-generated method stub
+           char arr[]= {'C','o','d','i','n','g'};
+           String str="Coding";
+           String str1= "  ";
+           System.out.println(str.length());
+           System.out.println(str1.length());
+           //System.out.println(str.charAt(3));
+           
+        }
+    
+    }
+    
